ee3bafe06d7b1d2e1200a0572b042b67b6fa6a72
Jonathan D. Forrider <jd4rider@gmail.com>
2018-09-06 03:13:50 -0600

    Plugin BoilerPlate

--- test-plugin.php
+++ test-plugin.php
@@ -30,16 +30,46 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+
+ // If this file is called Incorrectly, abort.
 defined('ABSPATH') or die('This file cannot be accessed in this manner');
 
+// Rquire Composer Autoload
 if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
     require_once dirname(__FILE__).'/vendor/autoload.php';
 }
 
+// Define Plugin CONSTS
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
-
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN', plugin_basename(__FILE__));
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+/**
+ * The code that runs during plugin activation
+ *
+ * @return void
+ */
+function activate_test_plugin(){
+    Activate::activate();
+}
+register_activation_hook( __FILE__, 'activate_test_plugin' );
+
+/**
+ * The code that runs during plugin deactivation
+ *
+ * @return void
+ */
+function deactivate_test_plugin(){
+    Deactivate::deactivate();
+}
+register_deactivation_hook( __FILE__, 'deactivate_test_plugin' );
+
+/**
+ * Initialize all the core classes of the plugin
+ */
 if(class_exists('Inc\\Init')){
     Inc\Init::register_services();
 }
